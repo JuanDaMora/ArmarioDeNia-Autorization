2bceac41af86e8fbb1b8bb11677b840e160eceb2
Juan Morantes <judamov.09@gmail.com>
2023-01-31 20:29:20 -0500

    first commit

---
+++ src/main/java/com/example/autorization/ArmarioDeNiaApplication.java
@@ -0,0 +1,13 @@
+package com.example.autorization;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArmarioDeNiaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ArmarioDeNiaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/autorization/ArmarioDeNiaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.autorization;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ArmarioDeNiaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
